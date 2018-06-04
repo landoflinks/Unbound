d45f2bb583604d48ea0ea507b8c8d80642a73372
= <lfschultz@ymail.com>
2018-06-03 22:20:35 -0500

    Implemented the base class (BaseProperty.cs) that implements INotifyPropertyChanged.

---
+++ Unbound/ViewModels/BaseProperty.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Unbound.ViewModels
+{
+    public abstract class BaseProperty : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
