d9b910f03ad002eaaccd34ed71ca8a51f78197ee
= <lfschultz@ymail.com>
2018-09-03 17:05:16 -0500

    Added a model reference to help switch map modes.

---
+++ Unbound/Models/MapChange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unbound.Models
+{
+    public class MapChange
+    {
+        private readonly Func<string, string> _mapMode;
+
+        public MapChange(Func<string, string> mapMode)
+        {
+            _mapMode = mapMode;
+        }
+
+        public string ConvertText(string mode)
+        {
+            return _mapMode(mode);
+        }
+    }
+}
