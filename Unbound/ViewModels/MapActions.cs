c4a5a2a1b50a9f225f3b89dd40e035fb43eca18f
= <lfschultz@ymail.com>
2018-09-03 17:59:08 -0500

    Changes to try to conform to MVVM; added MapActions.cs to control functions for main XAML window.

---
+++ Unbound/ViewModels/MapActions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace Unbound.ViewModels
+{
+    public class MapActions : BaseProperty
+    {
+        private void ChangeMapMode(Map name, string mapMode)
+        {
+            switch (mapMode)
+            {
+                case "Aerial":
+                    name.Mode = new AerialMode(false);
+                    break;
+                case "AerialWithLabels":
+                    name.Mode = new AerialMode(true);
+                    break;
+                case "Road":
+                    name.Mode = new RoadMode();
+                    break;
+                default:
+                    name.Mode = new RoadMode();
+                    break;
+            }
+        }
+    }
+}
