ecf004c5f26be33598e09ce3664835bf7437d381
= <lfschultz@ymail.com>
2018-09-30 22:09:31 -0500

    Fixing App.xaml references, a couple of very minor code changes.

--- Unbound/MainWindow.xaml.cs
+++ Unbound/MainWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Unbound
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
