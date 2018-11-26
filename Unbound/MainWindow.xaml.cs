cb5ac59e2c5af607e93b69b48845bdba5622465e
= <lfschultz@ymail.com>
2018-11-25 19:11:48 -0600

    Added some comments to better describe what's going on.

--- Unbound/MainWindow.xaml.cs
+++ Unbound/MainWindow.xaml.cs
@@ -8,17 +8,23 @@ namespace Unbound
         public MainWindow()
         {
             InitializeComponent();
+
+            // Sets focus on map (Allows use of +/- keys).
+            unboundMap.Focus();
         }
 
         private void btn_mapChange_Click(object sender, RoutedEventArgs e)
         {
+            // Switch between map types.
             if (unboundMap.Mode.ToString() == "Microsoft.Maps.MapControl.WPF.RoadMode")
             {
+                // Switch to Aerial/Satellite map with labels.
                 unboundMap.Mode = new AerialMode(true);
                 btn_mapChange.Content = "Road";
             }
             else
             {
+                // Switch to the standard road map.
                 unboundMap.Mode = new RoadMode();
                 btn_mapChange.Content = "Satellite";
             }
