d7f52ebc01eb97bf6c31a5b99631e08e9208da1b
= <lfschultz@ymail.com>
2018-12-29 17:30:22 -0600

    Redid some of the layout on MainWindow.xaml and added a Find button. Began adding code to grab the lat/lon information for a searched location or address using REST.

--- Unbound/MainWindow.xaml.cs
+++ Unbound/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Xml;
 using Microsoft.Maps.MapControl.WPF;
 
 namespace Unbound
@@ -11,7 +13,7 @@ namespace Unbound
         }
 
         // Switches between map layers.
-        private void btn_mapChange_Click(object sender, RoutedEventArgs e)
+        private void Btn_mapChange_Click(object sender, RoutedEventArgs e)
         {
             if (unboundMap.Mode.ToString() == "Microsoft.Maps.MapControl.WPF.RoadMode")
             {
@@ -28,15 +30,30 @@ namespace Unbound
         }
 
         // Manually zooms the map inward.
-        private void btn_zoomIn_Click(object sender, RoutedEventArgs e)
+        private void Btn_zoomIn_Click(object sender, RoutedEventArgs e)
         {
             unboundMap.ZoomLevel += 1;
         }
 
         // Manually zooms the map outward.
-        private void btn_zoomOut_Click(object sender, RoutedEventArgs e)
+        private void Btn_zoomOut_Click(object sender, RoutedEventArgs e)
         {
             unboundMap.ZoomLevel -= 1;
+        }
+
+        // Returns the latitude/longitude for a location.
+        private void MapSearch()
+        {
+            string address;
+            string mapKey = "AlUeSTQVv9GwuYMLV1Iyp3aiOgetXonrVPy8lFwNo5OBNqYQkKudpzbPm7FbURCg";
+
+            address = SearchTextBox.Text;
+
+            //Send REST request using the Microsoft Locations API.
+            string request = "http://dev.virtualearth.net/REST/v1/Locations/" + address + "?o=xml&key=" + mapKey;
+
+            //Grab the response.
+            //XmlDocument response = GetXmlResponse(request);
         }
     }
 }
