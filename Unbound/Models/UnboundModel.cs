8702e3e3bf0113e6353b28ba967c6d00dcea405b
= <lfschultz@ymail.com>
2019-04-14 15:24:16 -0500

    Removed the old ViewModel files and added the skeletons of new files for another try at MVVM.

---
+++ Unbound/Models/UnboundModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace Unbound.Models
+{
+    class UnboundModel
+    {
+
+    }
+}
---
+++ Unbound/ViewModels/BaseVM.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Unbound.ViewModels
+{
+    class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
